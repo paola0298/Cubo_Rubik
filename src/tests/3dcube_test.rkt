f531fc53d76ceed7254cde83b0f0b54a33ec17fa
Marlon Vega <mvegach2013@gmail.com>
2020-03-10 12:32:06 -0600

    Añadido test de cubo parcial en 3d

---
+++ src/tests/3dcube_test.rkt
@@ -0,0 +1,31 @@
+#lang racket
+ 
+(require pict3d
+         pict3d/universe)
+ 
+;; (current-material (material #:ambient 0.01
+;;                             #:diffuse 0.39
+;;                             #:specular 0.6
+;;                             #:roughness 0.2))
+ 
+(define lights+camera
+  (combine (light (pos 0 1 2) (emitted "Thistle"))
+           (light (pos 0 -1 -2) (emitted "PowderBlue"))
+           (basis 'camera (point-at (pos 4 4 0) origin))))
+
+;; (pos x y z)
+;; (with-color (rgba "color") (object position scale))
+;; (combine object1 object2 ... objectN)
+
+(define (on-draw s n t) 
+    (combine 
+        (rotate-z lights+camera (/ t 30)) 
+        (with-color (rgba "pink") (cube (pos 0 0 1) 1/2))
+        (with-color (rgba "orange") (cube (pos 0 0 -1) 1/2))
+        (with-color (rgba "white") (cube (pos 0 1 0) 1/2))
+        (with-color (rgba "yellow") (cube (pos 0 -1 0) 1/2))
+        (with-color (rgba "blue") (cube (pos 1 0 0) 1/2))
+        (with-color (rgba "green") (cube (pos -1 0 0) 1/2))
+        ))
+ 
+(big-bang3d 0 #:on-draw on-draw)
