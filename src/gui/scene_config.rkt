ba197ff9c5fc238186bb1380f9b582a7a7ba468e
Marlon Vega <mvegach2013@gmail.com>
2020-04-15 20:05:19 -0600

    Added scene configuration variables

---
+++ src/gui/scene_config.rkt
@@ -0,0 +1,13 @@
+;; ########## Variables del programa ##########
+
+;; Variable que define el tiempo que transcurre entre los pasos
+(define step-wait-time 3000)
+
+;; Variable que define la duración de la animación de un paso
+(define animation-duration 1500)
+
+;; Variable que define el incremento de coordenadas entre cubo y cubo
+(define coord-step 2);
+
+;; Variable para almacenar los cubos base del cubo
+(define base-cube '())
