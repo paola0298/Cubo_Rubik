a691a93731f52fe98d1156f7d498618a7d828fdd
Marlon Vega <mvegach2013@gmail.com>
2020-04-15 20:06:45 -0600

    Added placeholder functions

--- src/gui/secuencial.rkt
+++ src/gui/secuencial.rkt
@@ -5,36 +5,8 @@
     pict3d/universe)
 
 (include "../logic/cube_state.rkt")
-
-(current-material 
-    (material 
-        #:ambient 0.5
-        #:diffuse 0.4
-        #:specular 0.1
-        #:roughness 0.5))
-
-;; Cámara de la escena
-(define lights-camera
-    (combine 
-        (light (pos 5 0 0) (emitted "white" 25))
-        (light (pos 0 5 0) (emitted "white" 25))
-        (light (pos 0 0 5) (emitted "white" 25))
-        (basis 'camera (point-at (pos 5 5 4) origin))))
-
-;; Ejes de coordenadas para referencia
-(define coords
-    (combine
-        (with-color (rgba "red") (arrow origin (pos 0 0 4)))
-        (with-color (rgba "green") (arrow origin (pos 0 4 0)))
-        (with-color (rgba "blue") (arrow origin (pos 4 0 0)))
-    ))
-
-;; Variable que define el incremento de coordenadas entre cubo y cubo
-(define coord-step 2);
-
-;; Variable para almacenar los cubos base del cubo
-(define base-cube '())
-
+(include "scene_constants.rkt")
+(include "scene_config.rkt")
 
 ;; Funciones para modificar las coordenadas dadas en la dirección especificada
 (define (coord_up coords)
@@ -55,8 +27,19 @@
 (define (coord_front coords)
     (list (car coords) (+ (cadr coords) coord-step) (caddr coords)))
 
+;; Función para multiplicar las coordenadas dadas por un número dado
+(define (multiply_coords coords multiplier)
+    (list
+        (* (car coords) multiplier)
+        (* (cadr coords) multiplier)
+        (* (caddr coords) multiplier) 
+    ))
 
-;; Funciones para generar la matriz de coordenadas de cada cara
+;; Función para calcular las coordenadas de las caras del cubo (BASE)
+(define (calculate_cube_coords)
+    (#t))
+
+;; Funciones para generar la matriz de coordenadas de cada cara (BASE)
 (define (gen_top_coords)
     (#f))
 
@@ -75,37 +58,17 @@
 (define (gen_front_coords)
     (#f))
 
-;; Función para multiplicar las coordenadas dadas por un número dado
-(define (multiply_coords coords multiplier)
-    (list
-        (* (car coords) multiplier)
-        (* (cadr coords) multiplier)
-        (* (caddr coords) multiplier) 
-    ))
+;; Función para generar los cubos de colores con una matriz de coordenadas y una matriz de colores
+(define (generate_color_cubes coords_matrix face_matrix)
+    (#f))
 
-;; Función para calcular las coordenadas de las caras del cubo
-(define (calculate_faces_coords)
-    (#t))
+;; Función de renderizado principal
+(define (render_cube cube_size)
+    ())
 
 ;; Función que se llama cada cuadro para actualizar los gráficos
 (define (on-draw s n t)
-    (combine
-        (rotate-z lights-camera (/ t 90))
-        
-        (with-color (rgba "red") (cube (pos 1 1 1) 1))
-        (with-color (rgba "green") (cube (pos -1 -1 1) 1))
-        (with-color (rgba "blue") (cube (pos 1 -1 1) 1))
-        (with-color (rgba "white") (cube (pos -1 1 1) 1))
-
-        (with-color (rgba "white") (cube (pos 1 1 3) 1))
-        (with-color (rgba "blue") (cube (pos -1 -1 3) 1))
-        (with-color (rgba "green") (cube (pos 1 -1 3) 1))
-        (with-color (rgba "red") (cube (pos -1 1 3) 1))
-
-        (with-color (rgba "red") (arrow origin (pos 0 0 4)))
-        (with-color (rgba "green") (arrow origin (pos 0 4 0)))
-        (with-color (rgba "blue") (arrow origin (pos 4 0 0)))
-    ))
+    (render_cube cube_size))
 
 ;; Llamada a la función principal
 (big-bang3d 0 #:on-draw on-draw)
