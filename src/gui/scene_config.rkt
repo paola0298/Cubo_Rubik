cc950147b87c4c7832f5a5075f9379e21b717ce7
Marlon Vega <mvegach2013@gmail.com>
2020-04-19 23:58:09 -0600

    Added variables to save states and update them

--- src/gui/scene_config.rkt
+++ src/gui/scene_config.rkt
@@ -9,5 +9,14 @@
 ;; Variable que define el incremento de coordenadas entre cubo y cubo
 (define coord-step 2);
 
-;; Variable para almacenar los cubos base del cubo
-(define base-cube '())
+;; Variable que define el tamaño por defecto de los cubo base
+(define base-cube-size 0.96)
+
+;; Variable que define el tamaño por defecto de los cubos de color
+(define color-cube-size 4/5)
+
+;; Variable que almacena el estado actual del cubo
+(define current-state empty-pict3d)
+
+;; Variable que indica si se debe actualizar el estado gráfico del cubo.
+(define update_state #t)
