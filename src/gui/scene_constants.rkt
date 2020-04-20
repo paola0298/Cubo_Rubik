1a0e355cbdf6971b85c25be876e435c545289801
Marlon Vega <mvegach2013@gmail.com>
2020-04-19 23:58:41 -0600

    Modified light distance and intensity

--- src/gui/scene_constants.rkt
+++ src/gui/scene_constants.rkt
@@ -1,22 +1,17 @@
-(current-material 
-    (material 
-        #:ambient 0.5
-        #:diffuse 0.4
-        #:specular 0.1
-        #:roughness 0.5))
+
 
 ;; Cámara de la escena
 (define lights-camera
     (combine 
-        (light (pos 5 0 0) (emitted "white" 25))
-        (light (pos 0 5 0) (emitted "white" 25))
-        (light (pos 0 0 5) (emitted "white" 25))
-        (basis 'camera (point-at (pos 5 5 4) origin))))
+        (light (pos 20 0 0) (emitted "white" 50))
+        (light (pos 0 20 0) (emitted "white" 50))
+        (light (pos 0 0 20) (emitted "white" 50))
+        (basis 'camera (point-at (pos 14 14 14) origin))))
 
 ;; Ejes de coordenadas para referencia
 (define coords
     (combine
-        (with-color (rgba "red") (arrow origin (pos 0 0 4)))
-        (with-color (rgba "green") (arrow origin (pos 0 4 0)))
-        (with-color (rgba "blue") (arrow origin (pos 4 0 0)))
+        (with-color (rgba "red") (arrow origin (pos 0 0 10)))
+        (with-color (rgba "green") (arrow origin (pos 0 10 0)))
+        (with-color (rgba "blue") (arrow origin (pos 10 0 0)))
     ))
