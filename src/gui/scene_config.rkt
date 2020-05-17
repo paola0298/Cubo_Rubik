f11df42438feb7f7b7b456d42ccc766ad4e779bb
Marlon Vega <mvegach2013@gmail.com>
2020-05-17 12:27:01 -0600

    Fixed variables

--- src/gui/scene_config.rkt
+++ src/gui/scene_config.rkt
@@ -9,10 +9,8 @@
 ;; ##### Variables de configuración #####
 ;; Variable que define el incremento de coordenadas entre cubo y cubo
 (define coord-step 2);
-
 ;; Variable que define el incremento de coordenadas entre el cubo de color y el cubo base
 (define coord-step-color 0.30);
-
 ;; Variable que define el tamaño por defecto de los cubo base
 (define base-cube-size 0.96)
 ;; Variable que define el tamaño por defecto de los cubos de color
@@ -22,10 +20,6 @@
 ;; Variable que indica si se debe actualizar el estado gráfico del cubo.
 (define update_state #t)
 ;; Variable que indica el estado lógico del programa
-;; 0 => do-step
-;; 1 => rotating
-;; 2 => step-done
-;; 3 => delay-time
 (define logic-state 0)
 ;; Variable que define el ángulo actual de la rotación.
 (define rotation-angle 0)
@@ -33,19 +27,15 @@
 (define current-state '())
 ;; Variable que almacena el objeto pict3d del cubo.
 (define current-obj-state empty-pict3d)
-
+;; Variable que indica si el programa terminó de realizar los pasos.
 (define program-done #f)
+;; Variable que se utiliza para la rotación de la cámara al terminar de realizar los pasos.
 (define camera-rotation 0.0)
 
 ;; ##### Variables principales #####
 ;; Variable para almacenar el tamaño del cubo.
 (define cube-size 3)
 ;; Variable para almacenar el estado interno del cubo.
-;(define cube-internal-state '())
+(define cube-internal-state '())
 ;; Variable para almacenar los movimientos a realizar.
 (define cube-steps '("C0B" "F1I" "C2A" "C0B" "F2D"))
-
-; TEST
-(define cube-internal-state
-    'with
-)
